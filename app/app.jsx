b0a04f42d1d654674aaa15d8ec7b91ade026daf5
Tim <tuan.mrbean@gmail.com>
2016-09-29 11:12:29 +0700

    add files

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render(
+  <div>
+      <h1>React Boilerplate!</h1>
+  </div>
+  ,
+  document.getElementById('app')
+);
